5e994001290fc4c6f42fbc988347b8ed09af3a30
roshan393 <chanantony29@gmail.com>
2025-06-08 13:47:12 +0545

    header set

--- frontend/lcob/src/app/app.routes.ts
+++ frontend/lcob/src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout'; // a new wrapper component
+import { Home } from './user/home/home';
+import { Login } from './auth/login/login';
+import { Signup } from './auth/signup/signup';
+import { Shop } from './user/shop/shop';
+import { Product } from './user/product/product';
+import { Checkout } from './user/checkout/checkout';
+import { Success } from './user/success/success';
+import { Myorders } from './user/myorders/myorders';
+import { Dashboard } from './seller/dashboard/dashboard';
+import { Orders } from './seller/orders/orders';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // MAIN ROUTES (with header/footer)
+  {
+    path: '',
+    component: MainLayout,  // This wraps header/footer
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: Home, title: 'Home' },
+      { path: 'shop', component: Shop, title:'Shop' },
+      { path: 'shop/product/:id', component: Product, title: 'Product' },
+      { path: 'shop/checkout', component: Checkout, title: 'Checkout' },
+      { path: 'shop/success', component: Success, title: 'Success' },
+      { path: 'shop/my-orders', component: Myorders, title: 'My Orders' },
+    ],
+  },
+
+  // AUTH ROUTES (no layout)
+  { path: 'login', component: Login, title: 'Login' },
+  { path: 'signup', component: Signup, title: 'Signup' },
+
+  // SELLER ROUTES (no layout)
+  {
+    path: 'seller',
+    children: [
+      { path: 'login', component: Login },
+      { path: 'dashboard', component: Dashboard, title: 'Dashboard' },
+      { path: 'orders', component: Orders, title: 'Orders' },
+    ],
+  },
+
+  // WILDCARD (404)
+  { path: '**', redirectTo: 'home' },
+];
--- frontend/lcob/src/app/user/home/home.ts
+++ frontend/lcob/src/app/user/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+   
+   
+
 
 }
