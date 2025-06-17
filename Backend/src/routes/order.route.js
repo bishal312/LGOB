562816dfcdaa98642225d0fc348e1e7480c489c2
Bsal <bishalm626@gmail.com>
2025-06-17 16:59:57 +0545

    small change in autocomplete

--- Backend/src/routes/order.route.js
+++ Backend/src/routes/order.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { placeOrder } from "../controllers/order.controller.js";
+import { autoCompleteAddress, placeOrder } from "../controllers/order.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/place", protectRoute, placeOrder);
+router.post("/autocomplete", protectRoute, autoCompleteAddress);
 
 export default router;
