44a1881588b6fb8b32278ee07afd97d5e6af73ec
Bsal <bishalm626@gmail.com>
2025-06-27 21:59:21 +0545

    cookies

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -20,6 +20,7 @@ const PORT = process.env.PORT || 5002;
 // const __dirname = path.dirname(__filename);
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
@@ -30,7 +31,6 @@ app.use(
     credentials: true,
   })
 );
-app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
